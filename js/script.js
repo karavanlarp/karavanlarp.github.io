4f8c728c6f8e695f12ba5f2e7243e85837930487
mlutsar <meryllion@gmail.com>
2017-08-18 09:23:07 +0300

    mildly better responsive design

--- js/script.js
+++ js/script.js
@@ -1,41 +1,70 @@
 ﻿function dropWorld() {
-	hideDropdownContents();
-	document.getElementById("woldDropdown").classList.toggle("show");
+	hideDropdownContents("woldDropdown");
+	dropdown = document.getElementById("woldDropdown");
+	toggleVisibility(dropdown);
 }
 
 function dropPractical() {
-	hideDropdownContents();
-	document.getElementById("practicalDropdown").classList.toggle("show");
+	hideDropdownContents("practicalDropdown");
+	dropdown = document.getElementById("practicalDropdown");
+	toggleVisibility(dropdown);
 }
 
 function dropReg() {
-	hideDropdownContents();
-	document.getElementById("regDropdown").classList.toggle("show");
+	hideDropdownContents("regDropdown");
+	dropdown = document.getElementById("regDropdown");
+	toggleVisibility(dropdown);
 }
 
 function dropEvent() {
-	hideDropdownContents();
-	document.getElementById("eventDropdown").classList.toggle("show");
+	hideDropdownContents("eventDropdown");
+	dropdown = document.getElementById("eventDropdown");
+	toggleVisibility(dropdown);
 }
 
-function hideDropdownContents() {
+function hideDropdownContents(name) {
 	var drops = document.getElementsByClassName("dropdown-content");
+	
 	var idx;
 	for (idx = 0; idx < drops.length; idx++) {
-		drops[idx].classList.remove('show');
+		if(drops[idx].id !== name) {
+			drops[idx].style.display = 'none';
+		}
 	}
 }
 
+function showMenu() {
+	var menu = document.getElementsByClassName("innertube")[0];
+	toggleVisibility(menu);
+}
+
+function toggleVisibility(x) {
+	console.debug(x.style.display);
+	if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else if (x.style.display === 'none' || x.style.display === '') {
+        x.style.display = 'block';
+    }
+}
+
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (event.target.className !== 'dropbtn') {
 
 	var dropdowns = document.getElementsByClassName("dropdown-content");
 	var i;
 	for (i = 0; i < dropdowns.length; i++) {
 	  var openDropdown = dropdowns[i];
-	  if (openDropdown.classList.contains('show')) {
-		openDropdown.classList.remove('show');
+	  if (openDropdown.style.display !== 'none') {
+		openDropdown.style.display = 'none';
 	  }
 	}
   }
+}
+
+//TODO: WHY does it not toggle correctly for the first click on page??
+//this terrible hack makes it work. For now.
+window.onload = function() {
+	hideDropdownContents("thisIdDoesNotExist");
+	
+	var menu = document.getElementsByClassName("innertube")[0];
 }
